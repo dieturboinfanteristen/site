ef4ba9c45ca98728a6d47c08dd592df86f3144af
dieturboinfanteristen <DieTurboinfanteristen@gmx.de>
2023-11-13 16:12:07 +0100

    enene

--- Login/js/login.js
+++ Login/js/login.js
@@ -1,37 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("loginForm");
+    document.addEventListener("DOMContentLoaded", function () {
+        const form = document.getElementById("loginForm");
+        const loginButton = document.querySelector(".login__button");
+        const errorMessage = document.getElementById("error-message");
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
 
-    const emailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("password");
+            const emailInput = document.getElementById("email");
+            const passwordInput = document.getElementById("password");
 
-    const email = emailInput.value;
-    const password = passwordInput.value;
+            const email = emailInput.value;
+            const password = passwordInput.value;
 
-    const users = [
-      { email: "max@example.com", password: "Max" },
-      { email: "leon@example.com", password: "Leon" },
-      { email: "jendrigo@example.com", password: "Jendrigo" }
-    ];
+            const users = [
+                { email: "max@example.com", password: "Max" },
+                { email: "leon@example.com", password: "Leon" },
+                { email: "jendrigo@example.com", password: "Jendrigo" }
+            ];
 
-    // Sicherheitshinweis: In der Praxis sollten Passwörter gehasht und sicher in einer Datenbank gespeichert werden.
+            const user = users.find(user => user.email === email && user.password === password);
 
-    const user = users.find(user => user.email === email && user.password === password);
+            if (user) {
+                console.log("Erfolgreich eingeloggt! Benutzer:", user);
 
-    if (user) {
-      console.log("Erfolgreich eingeloggt! Benutzer:", user);
-
-      // Sicherheitshinweis: In der Praxis sollte ein sicherer Token generiert und vom Server zurückgegeben werden.
-      // Hier wird nur die E-Mail-Adresse zu Demonstrationszwecken im Local Storage gespeichert.
-      localStorage.setItem("loggedInEmail", email);
-
-      // Pfade für die Weiterleitung sollten in einer produktiven Umgebung angepasst werden.
-      window.location.href = "index.html";  // Passe den Pfad entsprechend deiner Ordnerstruktur an.
-    } else {
-      const errorMessage = document.getElementById("error-message");
-      errorMessage.textContent = "Anmeldung fehlgeschlagen. Überprüfe deine E-Mail-Adresse und dein Passwort.";
-    }
-  });
-});
+                localStorage.setItem("loggedInEmail", email);
+                loginButton.style.display = "none"; // Hide the button on successful login
+                errorMessage.textContent = ""; // Clear any previous error messages
+                window.location.href = "index.html";  // Adjust the path based on your folder structure.
+            } else {
+                errorMessage.textContent = "Anmeldung fehlgeschlagen. Überprüfe deine E-Mail-Adresse und dein Passwort.";
+                loginButton.style.display = "block"; // Show the button on login failure
+            }
+        });
+    });
